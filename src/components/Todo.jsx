f0a35786b2d8f3244ed8ef9a245012784daa9869
gayath43 <gayatribaji@freshivores.com>
2024-11-05 16:51:40 +0530

    update

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,9 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import todo_icon from '../assets/todo_icon.png'
 import Todoitems from './Todoitems'
 
 const Todo = () => {
-const[todolist,settodolist]= useState([]);
+const[todolist,settodolist]= useState(localStorage.getItem("todos")? JSON.parse(localStorage.getItem("todos")):[]);
 const inputRef=useRef();
 const add =() =>{ 
  
@@ -22,7 +22,19 @@ const add =() =>{
 const deleteTodo=(id)=>{
   settodolist((prvTodos)=>{ return prvTodos.filter((todo)=>todo.id !==id)})
 }
-
+const toggle=(id)=>{
+  settodolist((prevTodos)=>{
+    return prevTodos.map((todo)=>{
+      if(todo.id===id){
+        return{...todo,isComplete: !todo.isComplete}
+}
+return todo;
+    })
+  })
+}
+useEffect(()=>{
+  localStorage.setItem("todos",todolist,JSON.stringify(todolist))
+},[todolist])
   return (
     <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
         <div className='flex items-center mt-7 gap-2'> <img className='w-8' src={todo_icon } alt=""/>
@@ -36,7 +48,7 @@ const deleteTodo=(id)=>{
 
     <div>
       {todolist.map((item,index)=>{
-      return <Todoitems key={index} text={item.text} id={item.id} isComplete={item.isComplete} deleteTodo={deleteTodo}/>
+      return <Todoitems key={index} text={item.text} id={item.id} isComplete={item.isComplete} deleteTodo={deleteTodo}toggle={toggle}/>
       })}
       
     </div>
